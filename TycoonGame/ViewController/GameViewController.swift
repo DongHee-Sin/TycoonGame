ac6314aa5d2a5eff27d270855d5d78a47b7e97be
DongHee-Sin <voslr0601@naver.com>
2022-02-28 16:03:36 +0900

    1. 게임시작 버튼을 누르면 1초 뒤 손님이 등장 2. 손님이 나올 때 랜덤 숫자(1~6)의 주문을 함

--- TycoonGame/ViewController/GameViewController.swift
+++ TycoonGame/ViewController/GameViewController.swift
@@ -19,6 +19,20 @@ class GameViewController: UIViewController {
         case 손
     }
     
+    // 빵틀 상태 열거형
+    enum TrayState {
+        case 비어있음
+        case 반죽
+        case 팥
+        case 뒤집기1
+        case 뒤집기2
+        case 뒤집기3
+        case 뒤집기4
+        case 뒤집기가능
+        case 탐
+    }
+    
+    
     
     
     // MARK: - 전역변수
@@ -26,7 +40,11 @@ class GameViewController: UIViewController {
     var heartPoint: Int = 3
     var score: Int = 0
     
+    // 고객이 주문한 붕어빵 수량
+    var orderCount: Int?
     
+    // 빵틀 상태
+    var currentTrayState: [TrayState] = [.비어있음, .비어있음, .비어있음, .비어있음, .비어있음, .비어있음]
     
     
     
@@ -34,6 +52,9 @@ class GameViewController: UIViewController {
     
     // MARK: - UI 연결
     
+    // 손님 View
+    @IBOutlet weak var customerUIView: UIView!
+    @IBOutlet weak var customerOrder: UILabel!
     
     
     
@@ -42,8 +63,42 @@ class GameViewController: UIViewController {
     
     
     
-    
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 처음 시작할 때 손님 View 히든처리
+        customerViewHidden(true)
+    }
+    
+    
+
+    
+    // MARK: - View Did Appear
+    override func viewDidAppear(_ animated: Bool) {
+        // 1초 뒤 게임시작
+        sleep(1)
+        customerViewHidden(false)
+    }
+    
+    
+    
+    
+    
+    
+    // MARK: - 함수 정의
+    
+    // 손님 이미지를 히든처리 / false = 주문수량도 설정
+    func customerViewHidden(_ to: Bool) {
+        customerUIView.isHidden = to
+        if to == false {
+            orderCount = getRandomNumber()
+            customerOrder.text = "붕어빵 \(orderCount!)개 주세요."
+        }
+    }
+    
+    // 랜덤 숫자 반환 (1~6)
+    func getRandomNumber() -> Int {
+        return Int.random(in: 1...6)
     }
 }
